89b07932cb4501dcc72bd9b088ee9a81441027c4
Zhandra Habig <Zhandra Habig>
2017-04-23 10:48:14 +0800

    admin

--- message/send_oneway.php
+++ message/send_oneway.php
@@ -1,15 +1,17 @@
 <?php
+session_start();
+include("../include/configuration.php");
+include("../include/general_functions.php");
+
 if($_POST){
 	switch(strtolower($_POST['action'])){
 		case "sendmsg":
-
 			$parameters = array(
 			    'apikey' => 'eb8d6ed63c89ee953de368b110328f18', //API KEY
 			    'number' => '09177629194',
-			    'message' => 'Hi Fella! We just want to inform you about our new system that will surely help you. If you have other question you can just message us through some codes. Thank you!',
-			    
+			    'message' => urldecode($_POST['message']),
 			    'sendername' => 'SEMAPHORE'
-			);
+			);			
 			$response = sendViaSemaphore($parameters);
 			if(!empty($response) && strtolower($response[0]->status) == 'queued'){
 				echo "<div style='text-align:center;'><h1>Message Sent!</h1></div>";
@@ -19,17 +21,4 @@ if($_POST){
 		break;
 	}
 }
-
-function sendViaSemaphore($parameters){
-	$ch = curl_init();
-	curl_setopt( $ch, CURLOPT_URL,'http://api.semaphore.co/api/v4/messages' );
-	curl_setopt( $ch, CURLOPT_POST, 1 );
-	curl_setopt( $ch, CURLOPT_POSTFIELDS, http_build_query( $parameters ) );
-	curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
-	$output = curl_exec( $ch );
-	curl_close ($ch);
-
-	return json_decode($output);
-}
-
 ?>
